55cc67cd5ee32721ec43c9440067d05343438a05
Cuong Le <cuong.lm@cs.ventures>
2024-10-24 17:10:37 +0700

    feat: props component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
+
+  get users() {
+    return DUMMY_USERS;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,46 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+// import { Component, computed, signal } from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// @Component({
+//   selector: 'app-user',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   selectedUser = signal(DUMMY_USERS[randomIndex]);
+//   imageSrc = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+//   // get imageSrc() {
+//   //   return 'assets/users/' + this.selectedUser().avatar;
+//   // }
+
+//   onSelectUser() {
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
+//   }
+// }
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  standalone: true,
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imageSrc = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input() avatarSrc!: string;
+  @Input() name!: String;
 
-  // get imageSrc() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
+  get imageSrc() {
+    return 'assets/users/' + this.avatarSrc;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    console.log(this.name);
   }
 }
